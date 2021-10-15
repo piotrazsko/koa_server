cc75b448b5fa02250097010ab996c07d3a94004b
Serj <piotrazsko@gail.com>
2021-10-15 13:49:01 +0300

    added new send maessages

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,10 +1,50 @@
 const fs = require("fs");
+var admin = require("firebase-admin");
+var serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    "https://europeanvisitong-default-rtdb.europe-west1.firebasedatabase.app",
+});
 
 function setUser(data) {
   let rawdata = fs.readFileSync("user.json");
   let users = JSON.parse(rawdata);
   let savedData = JSON.stringify(data);
   fs.writeFileSync("user.json", savedData);
+}
+
+function sendNotification(token) {
+  // This registration token comes from the client FCM SDKs.
+  const registrationToken = token.trim();
+
+  const message = {
+    notification: {
+      title: "$FooCorp up 1.43% on the day",
+      body:
+        "$FooCorp gained 11.80 points to close at 835.67, up 1.43% on the day.",
+    },
+
+    data: {
+      score: "850",
+      time: "2:45",
+    },
+    token: registrationToken,
+  };
+
+  // Send a message to the device corresponding to the provided
+  // registration token.
+  admin
+    .messaging()
+    .send(message)
+    .then((response) => {
+      // Response is a message ID string.
+      console.log("Successfully sent message:", response);
+    })
+    .catch((error) => {
+      console.log("Error sending message:", error);
+    });
 }
 
 let users = [
@@ -96,6 +136,6 @@ module.exports = {
     const data = {
       ...ctx.request.body,
     };
-    console.log(data);
+    sendNotification(data.token);
   },
 };
